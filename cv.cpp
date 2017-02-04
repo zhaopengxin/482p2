a305e6506dcafe56428cba281b56b61a45c3536a
Pengxin Zhao <zhaopx@loadtestp09.dc.umich.edu>
2017-02-04 12:39:50 -0500

    partially cv.cpp

--- cv.cpp
+++ cv.cpp
@@ -1,4 +1,6 @@
 #include "cv.h"
+#include "mutex.h"
+#include "cpu.h"
 
 using namespace std;
 
@@ -17,13 +19,27 @@ void cv::wait(mutex&){
 	//a thread always calls wait() while holding a lock, and the call to wait atomically releases the lock
 	//and puts the threads on the condition variable's waiting queue.
 	cpu::interrupt_disable();
-	
+	mutex->unlock();
+	impl_ptr->waiting_queue.push(getcontext(ucontext_ptr));
+
+	cpu::interrupt_enable_suspend();
+	mutex->lock();
 }
 
 void cv::signal(){
-
+	cpu::interrupt_disable();
+	if(impl_ptr->waiting_queue.size() > 0){
+		ready_queue.push(impl_ptr->waiting_queue.pop());
+	}
+	cpu::interrupt_send();
+	cpu::interrupt_enable();
 }
 
 void cv::broadcast(){
-
+	cpu::interrupt_disable();
+	while(impl_ptr->waiting_queue.size() > 0){
+		ready_queue.push(impl_ptr->waiting_queue.pop());
+	}
+	cpu::interrupt_send();
+	cpu::interrupt_enable();
 }
